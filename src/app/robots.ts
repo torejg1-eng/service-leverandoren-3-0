cabdc8be346f3fd408b5fedd475064e6479504af
torejg1-eng <tore.j.g.1@gmail.com>
2026-09-02 19:35:53 +0200

    Rett SEO-adresser til offisielt domene

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://www.serviceleverandøren.no/sitemap.xml",
-    host: "https://www.serviceleverandøren.no",
+    sitemap: "https://www.service-leverandøren.no/sitemap.xml",
+    host: "https://www.service-leverandøren.no",
   };
 }
